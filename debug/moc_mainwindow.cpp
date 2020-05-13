78c0ebf599769e5fcad44cd1ea1aafcfccdfa9b1
Doaa Yehia <doaayramdan@gmail.com>
2020-05-13 04:16:59 +0200

    Fixed the deAllocation crash elhamdullellah + added the seg table for each process + replace deallocated process table with new process table

--- debug/moc_mainwindow.cpp
+++ debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[6];
-    char stringdata0[104];
+    QByteArrayData data[5];
+    char stringdata0[80];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,13 +36,11 @@ QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 25), // "on_add_total_size_clicked"
 QT_MOC_LITERAL(2, 37, 0), // ""
 QT_MOC_LITERAL(3, 38, 23), // "on_pushButton_4_clicked"
-QT_MOC_LITERAL(4, 62, 23), // "on_pushButton_8_clicked"
-QT_MOC_LITERAL(5, 86, 17) // "on_submit_clicked"
+QT_MOC_LITERAL(4, 62, 17) // "on_submit_clicked"
 
     },
     "MainWindow\0on_add_total_size_clicked\0"
-    "\0on_pushButton_4_clicked\0"
-    "on_pushButton_8_clicked\0on_submit_clicked"
+    "\0on_pushButton_4_clicked\0on_submit_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,7 +50,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,13 +58,11 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x08 /* Private */,
-       3,    0,   35,    2, 0x08 /* Private */,
-       4,    0,   36,    2, 0x08 /* Private */,
-       5,    0,   37,    2, 0x08 /* Private */,
+       1,    0,   29,    2, 0x08 /* Private */,
+       3,    0,   30,    2, 0x08 /* Private */,
+       4,    0,   31,    2, 0x08 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -82,8 +78,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->on_add_total_size_clicked(); break;
         case 1: _t->on_pushButton_4_clicked(); break;
-        //case 2: _t->on_pushButton_8_clicked(); break;
-        case 3: _t->on_submit_clicked(); break;
+        case 2: _t->on_submit_clicked(); break;
         default: ;
         }
     }
@@ -119,13 +114,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 3;
     }
     return _id;
 }
